c41ffffc1e0bff9a8bf7949512ea5ab1d3ff2937
Gabo03303 <114263795+Gabo03303@users.noreply.github.com>
2023-02-07 19:16:11 -0500

    Entregable 2

---
+++ src/MainC/MainPageC.jsx
@@ -0,0 +1,153 @@
+import { Link } from "react-router-dom"
+
+
+
+function MainPageC(){
+    
+    return <div>
+                
+                <ul className="list-group" style={{border: "hidden;", opacity:"0.7"}}>
+                    <li className="list-group-item" id="restaurantes1" style={{border: "hidden;"}}>
+                    <h5>Comida Rapida</h5>
+                    <div className="row justify-content-md-center">
+                        <div className="col-sm-3">
+                        <div className="card" style={{width: "18rem"}}>
+                            <div className="card-body">
+                            <h6 className="card-title" style={{textAlign: "center;"}}>Mc'Donalds</h6>
+                            <img src="/img/mcdonald_logo.png" className="card-img-top" style={{borderRadius:"150px;"}}/>
+                            <br/><br/><br/>
+                            <Link to={"#"} className="btn btn-primary">Ver Menu</Link>
+                            </div>
+                        </div>
+                        </div>
+                        <div className="col-sm-3">
+                        <div className="card" style={{width: "18rem"}}>
+                            <div className="card-body">
+                            <h6 className="card-title" style={{textAlign: "center;"}}>Bembos</h6>
+                            <img src="/img/bembos_logo.jpg" className="card-img-top" style={{borderRadius:"150px;"}}/>
+                            <br/><br/><br/>
+                            <Link to={"#"} className="btn btn-primary">Ver Menu</Link>
+                            </div>
+                        </div>
+                        </div>
+                        <div className="col-sm-3">
+                        <div className="card" style={{width: "18rem"}}>
+                            <div className="card-body">
+                            <h6 className="card-title" style={{textAlign: "center;"}}>KFC</h6>
+                            <img src="/img/KFC-Logo.png" className="card-img-top" style={{borderRadius:"150px;"}}/>
+                            <br/><br/><br/>
+                            <Link to={"#"} className="btn btn-primary">Ver Menu</Link>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    </li>  
+                    <li className="list-group-item" id="restaurantes2" style={{border: "hidden;"}}>
+                    <h5>Pastas y Pizzas</h5>
+                    <div className="row justify-content-md-center">
+                        <div className="col-sm-3">
+                        <div className="card" style={{width: "18rem"}}>
+                            <div className="card-body">
+                            <h6 className="card-title" style={{textAlign: "center;"}}>Pizza Hut</h6>
+                            <img src="/img/Pizza_Hut_Logo_3.png" className="card-img-top" style={{borderRadius:"150px;"}}/>
+                            <br/><br/><br/>
+                            <Link to={"#"} className="btn btn-primary">Ver Menu</Link>
+                            </div>
+                        </div>
+                        </div>
+                        <div className="col-sm-3">
+                        <div className="card" style={{width: "18rem"}}>
+                            <div className="card-body">
+                            <h6 className="card-title" style={{textAlign: "center;"}}>Papa Johns</h6>
+                            <img src="/img/papajh.jpg" className="card-img-top" style={{borderRadius:"150px;"}}/>
+                            <br/><br/><br/>
+                            <Link to={"#"} className="btn btn-primary">Ver Menu</Link>
+                            </div>
+                        </div>
+                        </div>
+                        <div className="col-sm-3">
+                        <div className="card" style={{width: "18rem"}}>
+                            <div className="card-body">
+                            <h6 className="card-title" style={{textAlign: "center;"}}>Mamma Tomato</h6>
+                            <img src="/img/mamatomato_logo.jpg" className="card-img-top" style={{borderRadius:"150px;"}}/>
+                            <br/><br/><br/>
+                            <Link to={"#"} className="btn btn-primary">Ver Menu</Link>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    </li>
+                    <li className="list-group-item" id="restaurantes3" style={{border: "hidden;"}}>
+                    <h5>Pollo</h5>
+                    <div className="row justify-content-md-center">
+                        <div className="col-sm-3">
+                        <div className="card" style={{width: "18rem"}}>
+                            <div className="card-body">
+                            <h6 className="card-title" style={{textAlign: "center;"}}>Norkys</h6>
+                            <img src="/img/Norkys-Logo.png" className="card-img-top" style={{borderRadius:"150px;"}}/>
+                            <br/><br/><br/>
+                            <Link to={"#"} className="btn btn-primary">Ver Menu</Link>
+                            </div>
+                        </div>
+                        </div>
+                        <div className="col-sm-3">
+                        <div className="card" style={{width: "18rem"}}>
+                            <div className="card-body">
+                            <h6 className="card-title" style={{textAlign: "center;"}}>Pardos Chiken</h6>
+                            <img src="/img/pardos_logo.jpg" className="card-img-top" style={{borderRadius:"150px;"}}/>
+                            <br/><br/><br/>
+                            <Link to={"#"} className="btn btn-primary">Ver Menu</Link>
+                            </div>
+                        </div>
+                        </div>
+                        <div className="col-sm-3">
+                        <div className="card" style={{width: "18rem"}}>
+                            <div className="card-body">
+                            <h6 className="card-title" style={{textAlign: "center;"}}>La Leña</h6>
+                            <img src="/img/leña_logo.png" className="card-img-top" style={{borderRadius:"150px;"}}/>
+                            <br/><br/><br/>
+                            <Link to={"#"} className="btn btn-primary">Ver Menu</Link>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    </li>
+                    <li className="list-group-item" id="restaurantes4" style={{border: "hidden;"}}>
+                    <h5>Pescado y Mariscos</h5>
+                    <div className="row justify-content-md-center">
+                        <div className="col-sm-3">
+                        <div className="card" style={{width: "18rem"}}>
+                            <div className="card-body">
+                            <h6 className="card-title" style={{textAlign: "center;"}}>Rosa Nautica</h6>
+                            <img src="/img/rosa_logo.png" className="card-img-top" style={{borderRadius:"150px;"}}/>
+                            <br/><br/><br/>
+                            <Link to={"#"} className="btn btn-primary">Ver Menu</Link>
+                            </div>
+                        </div>
+                        </div>
+                        <div className="col-sm-3">
+                        <div className="card" style={{width: "18rem"}}>
+                            <div className="card-body">
+                            <h6 className="card-title" style={{textAlign: "center;"}}>Mi Barrunto</h6>
+                            <img src="/img/barrunto_logo.jpg" className="card-img-top" style={{borderRadius:"150px;"}}/>
+                            <br/><br/><br/>
+                            <Link to={"#"} className="btn btn-primary">Ver Menu</Link>
+                            </div>
+                        </div>
+                        </div>
+                        <div className="col-sm-3">
+                        <div className="card" style={{width: "18rem"}}>
+                            <div className="card-body">
+                            <h6 className="card-title" style={{textAlign: "center;"}}>Embarcadero 41</h6>
+                            <img src="/img/embarcadero_logo.png" className="card-img-top" style={{borderRadius:"150px;"}}/>
+                            <br/><br/><br/>
+                            <a href="/pantalla4" className="btn btn-primary">Ver Menu</a>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                </li>  
+                </ul>
+            </div>
+    }
+export default MainPageC
